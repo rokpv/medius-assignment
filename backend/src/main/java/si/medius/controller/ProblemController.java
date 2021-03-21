fe5e36622675210c21319c37bd90b251e9195a6e
Rok <rp8257@student.uni-lj.si>
2021-03-21 18:56:59 +0100

    Changed api exception types.

--- backend/src/main/java/si/medius/controller/ProblemController.java
+++ backend/src/main/java/si/medius/controller/ProblemController.java
@@ -2,13 +2,12 @@ package si.medius.controller;
 
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import si.medius.entity.Problem;
 import si.medius.entity.Solution;
-import si.medius.exception.BadRequestException;
 import si.medius.service.ProblemService;
 import si.medius.solver.LightsOutSolver;
 
@@ -32,34 +31,34 @@ public class ProblemController {
     }
 
     @GetMapping("/{id}")
-    public Problem getProblemById(@PathVariable @NonNull String id) throws BadRequestException {
+    public Problem getProblemById(@PathVariable @NonNull String id) throws HttpClientErrorException {
         try {
             return problemService.getProblemById(id);
         } catch (NotFoundException e) {
-            throw new BadRequestException(e.getMessage());
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
     }
 
     @PostMapping("/")
-    public Problem createProblem(@RequestBody Problem problem) throws BadRequestException {
+    public Problem createProblem(@RequestBody Problem problem) throws HttpClientErrorException {
         int size = problem.getSize();
         String description = problem.getDescription();
 
         if (size > 8 || size < 3) {
-            throw new BadRequestException("Problem size must be between 3 and 8.");
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Problem size must be between 3 and 8.");
         }
 
         if (description.length() != size * size) {
-            throw new BadRequestException("Description is not of correct dimensions. Expected " + size + "x" + "size.");
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Description is not of correct dimensions. Expected " + size + "x" + "size.");
         }
 
         if (!description.matches("^[01]*$")) {
-            throw new BadRequestException("Description must contain only the characters 0 and 1.");
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Description must contain only the characters 0 and 1.");
         }
 
         Solution solution = lightsOutSolver.solve(problem);
         if (solution == null) {
-            throw new BadRequestException("Given problem is unsolvable.");
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Given problem is unsolvable.");
         }
 
         return problemService.createProblem(problem);
